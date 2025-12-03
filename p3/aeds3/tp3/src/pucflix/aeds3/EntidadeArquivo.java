3445e61e270e553933db68856be9c190957aea47
mateushmd <mateushmdiniz@gmail.com>
2025-12-03 15:27:39 -0300

    leaving only ca stuff

--- p3/aeds3/tp3/src/pucflix/aeds3/EntidadeArquivo.java
+++
@@ -1,7 +0,0 @@
-package pucflix.aeds3;
-public interface EntidadeArquivo {
-    public void setID(int id);
-    public int getID();
-    public byte[] toByteArray() throws Exception;
-    public void fromByteArray(byte[] vb) throws Exception;
-}
